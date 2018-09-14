51469e0fa601ccb0662e297f303656b2cc0369d4
Bonda Sisephaputra <bonda.sisephaputra@gmail.com>
2018-09-14 08:36:57 +0700

    CI Framework Configuration

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
-$route['404_override'] = '';
+$route['default_controller'] = 'page/home';
+$route['page'] = 'page/home';
+$route['page/(:any)'] = 'page/$1';
+$route['search'] = 'search/home';
+$route['search/(:any)'] = 'search/$1';
+$route['404_override'] = 'page/home';
 $route['translate_uri_dashes'] = FALSE;
