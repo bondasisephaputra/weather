59a42bcb746ccdbc0297edb18022cd852d607c60
Bonda Sisephaputra <bonda.sisephaputra@gmail.com>
2018-09-14 20:27:21 +0700

    Update Search Template

---
+++ application/views/page/search.php
@@ -0,0 +1,118 @@
+<div class="content features-two">
+  <div class="container">
+    <div class="row" style="margin-bottom: 50px;">
+
+            <div class="col-md-offset-4 col-md-4 col-sm-offset-4 col-sm-4 col-xs-offset-4 col-xs-4">
+                  <div class="row">
+                     <!-- Staff name with designation. Designation should be enclosed inside <span> tag. -->
+                    <div class="col-md-4 col-sm-4 col-xs-4" style="text-align: right;padding-top:50px;color:#ffffff;">
+                      <p style="font-size: 40px;">12°</p>
+                      <br/>
+                      <p style="font-size: 15px;">
+                        <span style="color:#FBA93D;font-weight:bold;">22°</span> / <span style="color:#1C9ACA;font-weight:bold;">9°</span>
+                      </p>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-4" style="text-align: center;padding-left: 0px;margin-left: 0px;padding-right: 0px;margin-right: 0px;">
+                      <img src="https://www.metaweather.com/static/img/weather/c.svg" alt="" />
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-4" style="text-align: left;padding-top:80px;color:#ffffff;">
+                      <p style="font-size: 12px;">
+                        <span style="color:#ffffff;font-weight:bold;">RealFeel</span> <span style="color:#1C9ACA;font-weight:bold;">11°</span>
+                      </p>
+                    </div>
+                    <div class="col-md-12 col-sm-12 col-xs-12" style="text-align: center;padding-top:20px;color:#1C9ACA;">
+                      <p style="font-size: 50px;">Sunny</p>
+                    </div>
+                                         
+                  </div>
+                  
+            </div>
+
+    </div>
+
+    <div class="row">
+      <div class="col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10 col-xs-offset-1 col-xs-10">
+        <div style="text-align: left;margin-bottom: 10px;color: #ffffff;font-weight:bold;">          
+          <p><span style="color: #1C9ACA;font-weight:bold;">Wind</span> XXX &nbsp;&nbsp;&nbsp;<span style="color: #1C9ACA;font-weight:bold;">Rain</span> XXX &nbsp;&nbsp;&nbsp;<span style="color: #1C9ACA;font-weight:bold;">Storm</span> XXX</p>
+        </div>
+      </div>      
+    </div>
+
+
+    <div class="row">
+
+          <div class="col-md-1 col-sm-1 col-xs-1">
+          </div>      
+
+          <div class="col-md-2 col-sm-2 col-xs-2" style="background-color: #000000;opacity: 0.6;">
+            <div>
+              <h4 class="title"> Today</h4>
+              <div class="col-md-9 col-sm-9 col-xs-9">
+                <img src="https://www.metaweather.com/static/img/weather/c.svg" alt="" style="margin-bottom: 10px;margin-top: 10px;" />
+              </div>
+              <div class="col-md-3 col-sm-3 col-xs-3" style="text-align: left;margin-left: 0px;padding-left: 0px;padding-top: 10px; ">
+                <p style="color:#FBA93D;font-weight:bold;padding-bottom: 0px;margin-bottom: 0px;">22°</p>
+                <p style="color:#1C9ACA;font-weight:bold;padding-top: 0px;margin-top: 0px;">9°</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-2 col-sm-2 col-xs-2" style="background-color: #000000;opacity: 0.6;margin-left: 10px;">
+            <div>
+              <h4 class="title"> Today</h4>
+              <div class="col-md-9 col-sm-9 col-xs-9">
+                <img src="https://www.metaweather.com/static/img/weather/c.svg" alt="" style="margin-bottom: 10px;margin-top: 10px;" />
+              </div>
+              <div class="col-md-3 col-sm-3 col-xs-3" style="text-align: left;margin-left: 0px;padding-left: 0px;padding-top: 10px; ">
+                <p style="color:#FBA93D;font-weight:bold;padding-bottom: 0px;margin-bottom: 0px;">22°</p>
+                <p style="color:#1C9ACA;font-weight:bold;padding-top: 0px;margin-top: 0px;">9°</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-2 col-sm-2 col-xs-2" style="background-color: #000000;opacity: 0.6;margin-left: 10px;">
+            <div>
+              <h4 class="title"> Today</h4>
+              <div class="col-md-9 col-sm-9 col-xs-9">
+                <img src="https://www.metaweather.com/static/img/weather/c.svg" alt="" style="margin-bottom: 10px;margin-top: 10px;" />
+              </div>
+              <div class="col-md-3 col-sm-3 col-xs-3" style="text-align: left;margin-left: 0px;padding-left: 0px;padding-top: 10px; ">
+                <p style="color:#FBA93D;font-weight:bold;padding-bottom: 0px;margin-bottom: 0px;">22°</p>
+                <p style="color:#1C9ACA;font-weight:bold;padding-top: 0px;margin-top: 0px;">9°</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-2 col-sm-2 col-xs-2" style="background-color: #000000;opacity: 0.6;margin-left: 10px;">
+            <div>
+              <h4 class="title"> Today</h4>
+              <div class="col-md-9 col-sm-9 col-xs-9">
+                <img src="https://www.metaweather.com/static/img/weather/c.svg" alt="" style="margin-bottom: 10px;margin-top: 10px;" />
+              </div>
+              <div class="col-md-3 col-sm-3 col-xs-3" style="text-align: left;margin-left: 0px;padding-left: 0px;padding-top: 10px; ">
+                <p style="color:#FBA93D;font-weight:bold;padding-bottom: 0px;margin-bottom: 0px;">22°</p>
+                <p style="color:#1C9ACA;font-weight:bold;padding-top: 0px;margin-top: 0px;">9°</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-2 col-sm-2 col-xs-2" style="background-color: #000000;opacity: 0.6;margin-left: 10px;">
+            <div>
+              <h4 class="title"> Today</h4>
+              <div class="col-md-9 col-sm-9 col-xs-9">
+                <img src="https://www.metaweather.com/static/img/weather/c.svg" alt="" style="margin-bottom: 10px;margin-top: 10px;" />
+              </div>
+              <div class="col-md-3 col-sm-3 col-xs-3" style="text-align: left;margin-left: 0px;padding-left: 0px;padding-top: 10px; ">
+                <p style="color:#FBA93D;font-weight:bold;padding-bottom: 0px;margin-bottom: 0px;">22°</p>
+                <p style="color:#1C9ACA;font-weight:bold;padding-top: 0px;margin-top: 0px;">9°</p>
+              </div>
+            </div>
+          </div>
+
+         
+
+
+
+    </div>
+  </div>
+</div>
